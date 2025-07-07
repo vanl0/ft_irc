9af2590d4730d520b2cb11cd9b9b6f31a1ae93eb
pauldahacker <paul.demasca@gmail.com>
2025-07-07 17:41:50 +0200

    input check modified

--- Server.cpp
+++ Server.cpp
@@ -1,18 +1,28 @@
 #include "Server.hpp"
 
-void Server::ClearClients(int fd){ //-> clear the clients
-	for(size_t i = 0; i < fds.size(); i++){ //-> remove the client from the pollfd
+void Server::ClearClients(int fd)
+{ //-> clear the clients
+	for(size_t i = 0; i < fds.size(); i++)
+	{ //-> remove the client from the pollfd
 		if (fds[i].fd == fd)
-			{fds.erase(fds.begin() + i); break;}
+		{
+			fds.erase(fds.begin() + i);
+			break;
+		}
 	}
-	for(size_t i = 0; i < clients.size(); i++){ //-> remove the client from the vector of clients
+	for(size_t i = 0; i < clients.size(); i++)
+	{ //-> remove the client from the vector of clients
 		if (clients[i].GetFd() == fd)
-			{clients.erase(clients.begin() + i); break;}
+		{
+			clients.erase(clients.begin() + i);
+			break;
+		}
 	}
 
 }
 
 bool Server::Signal = false; //-> initialize the static boolean
+
 void Server::SignalHandler(int signum)
 {
 	(void)signum;
@@ -20,12 +30,15 @@ void Server::SignalHandler(int signum)
 	Server::Signal = true; //-> set the static boolean to true to stop the server
 }
 
-void	Server::CloseFds(){
-	for(size_t i = 0; i < clients.size(); i++){ //-> close all the clients
+void	Server::CloseFds()
+{
+	for(size_t i = 0; i < clients.size(); i++)
+	{ //-> close all the clients
 		std::cout << RED << "Client <" << clients[i].GetFd() << "> Disconnected" << WHI << std::endl;
 		close(clients[i].GetFd());
 	}
-	if (SerSocketFd != -1){ //-> close the server socket
+	if (SerSocketFd != -1)
+	{ //-> close the server socket
 		std::cout << RED << "Server <" << SerSocketFd << "> Disconnected" << WHI << std::endl;
 		close(SerSocketFd);
 	}
@@ -38,13 +51,14 @@ void Server::ReceiveNewData(int fd)
 
 	ssize_t bytes = recv(fd, buff, sizeof(buff) - 1 , 0); //-> receive the data
 
-	if(bytes <= 0){ //-> check if the client disconnected
+	if(bytes <= 0)
+	{ //-> check if the client disconnected
 		std::cout << RED << "Client <" << fd << "> Disconnected" << WHI << std::endl;
 		ClearClients(fd); //-> clear the client
 		close(fd); //-> close the client socket
 	}
-
-	else{ //-> print the received data
+	else
+	{ //-> print the received data
 		buff[bytes] = '\0';
 		std::cout << YEL << "Client <" << fd << "> Data: " << WHI << buff;
 		//here you can add your code to process the received data: parse, check, authenticate, handle the command, etc...
@@ -87,17 +101,17 @@ void Server::SerSocket()
 	add.sin_port = htons(this->Port); //-> convert the port to network byte order (big endian)
 
 	SerSocketFd = socket(AF_INET, SOCK_STREAM, 0); //-> create the server socket
-	if(SerSocketFd == -1) //-> check if the socket is created
-		throw(std::runtime_error("faild to create socket"));
+	if (SerSocketFd == -1) //-> check if the socket is created
+		throw(std::runtime_error("failed to create socket"));
 
-	if(setsockopt(SerSocketFd, SOL_SOCKET, SO_REUSEADDR, &en, sizeof(en)) == -1) //-> set the socket option (SO_REUSEADDR) to reuse the address
-		throw(std::runtime_error("faild to set option (SO_REUSEADDR) on socket"));
-	 if (fcntl(SerSocketFd, F_SETFL, O_NONBLOCK) == -1) //-> set the socket option (O_NONBLOCK) for non-blocking socket
-		throw(std::runtime_error("faild to set option (O_NONBLOCK) on socket"));
+	if (setsockopt(SerSocketFd, SOL_SOCKET, SO_REUSEADDR, &en, sizeof(en)) == -1) //-> set the socket option (SO_REUSEADDR) to reuse the address
+		throw(std::runtime_error("failed to set option (SO_REUSEADDR) on socket"));
+	if (fcntl(SerSocketFd, F_SETFL, O_NONBLOCK) == -1) //-> set the socket option (O_NONBLOCK) for non-blocking socket
+		throw(std::runtime_error("failed to set option (O_NONBLOCK) on socket"));
 	if (bind(SerSocketFd, (struct sockaddr *)&add, sizeof(add)) == -1) //-> bind the socket to the address
-		throw(std::runtime_error("faild to bind socket"));
+		throw(std::runtime_error("failed to bind socket"));
 	if (listen(SerSocketFd, SOMAXCONN) == -1) //-> listen for incoming connections and making the socket a passive socket
-		throw(std::runtime_error("listen() faild"));
+		throw(std::runtime_error("listen() failed"));
 
 	NewPoll.fd = SerSocketFd; //-> add the server socket to the pollfd
 	NewPoll.events = POLLIN; //-> set the event to POLLIN for reading data
@@ -111,16 +125,18 @@ void Server::ServerInit(int port, const std::string &pass)
     this->password = pass;
 	SerSocket(); //-> create the server socket
 
-	std::cout << GRE << "IRC Server <" << SerSocketFd << "> Connected on port [" << this->Port << "]" << WHI << std::endl;
-	std::cout << "Waiting to accept a connection...\n";
+	std::cout << GRE << "IRC server <" << SerSocketFd << "> connected on port [" << this->Port << "]" << WHI << std::endl;
+	std::cout << "Waiting to accept a new connection...\n";
 
 	while (Server::Signal == false){ //-> run the server until the signal is received
 
-		if((poll(&fds[0],fds.size(),-1) == -1) && Server::Signal == false) //-> wait for an event
-			throw(std::runtime_error("poll() faild"));
+		if ((poll(&fds[0],fds.size(),-1) == -1) && Server::Signal == false) //-> wait for an event
+			throw(std::runtime_error("poll() failed"));
 
-		for (size_t i = 0; i < fds.size(); i++){ //-> check all file descriptors
-			if (fds[i].revents & POLLIN){ //-> check if there is data to read
+		for (size_t i = 0; i < fds.size(); i++)
+		{ //-> check all file descriptors
+			if (fds[i].revents & POLLIN)
+			{ //-> check if there is data to read
 				if (fds[i].fd == SerSocketFd)
 					AcceptNewClient(); //-> accept new client
 				else
--- main.cpp
+++ main.cpp
@@ -19,44 +19,41 @@
 #define GRE "\e[1;32m" //-> for green color
 #define YEL "\e[1;33m" //-> for yellow color
 
-static int validateInputPort(const std::string &inputPort)
+static void validateInput(int argc, char *argv[])
 {
+	if (argc != 3)
+		throw(std::runtime_error("[INPUT] Incorrect number of parameters"));
+	std::string inputPort(argv[1]);
+	std::string inputPass(argv[2]);
 	for (size_t i = 0; i < inputPort.size(); i++)
 	{
 		if (!std::isdigit(inputPort[i]))
-		{
-			std::cerr << "Input error! The port must be a number." << std::endl;
-			exit(1);
-		}
+			throw(std::runtime_error("[INPUT] The port must be a number ."));
 	}
 	long portNum = std::atol(inputPort.c_str());
 	if (portNum < 1024 || portNum > 65535)
-	{
-		std::cerr << "Input error! The port must be between 1024 and 65535." << std::endl;
-		exit(1);
-	}
-	return (static_cast<int>(portNum));
+		throw(std::runtime_error("[INPUT] The port must be between 1024 and 65535."));
+	if (inputPass.empty())
+		throw(std::runtime_error("[INPUT] The password cannot be empty."));
 }
 
 int main(int argc, char *argv[])
 {
-	if (argc != 3)
-	{
-		std::cerr << "Input error! Usage: ./ircserv [port] [password]" << std::endl;
-		return (1);
-	}
-	int port = validateInputPort(argv[1]);
-	std::string pass(argv[2]);
 	Server ser;
 	std::cout << "---- SERVER ----" << std::endl;
-	try{
+	try
+	{
+		validateInput(argc, argv);
+		int port = std::atoi(argv[1]);
+		std::string pass(argv[2]);
 		signal(SIGINT, Server::SignalHandler); //-> catch the signal (ctrl + c)
 		signal(SIGQUIT, Server::SignalHandler); //-> catch the signal (ctrl + \)
 		ser.ServerInit(port, pass); //-> initialize the server
 	}
-	catch(const std::exception& e){
+	catch(const std::exception& e)
+	{
 		ser.CloseFds(); //-> close the file descriptors
 		std::cerr << e.what() << std::endl;
 	}
-	std::cout << "The Server Closed!" << std::endl;
+	std::cout << "The IRC server closed!" << std::endl;
 }
