0cc409fd239c74665b0a384f8966431cc9770aa4
Ivan Lorenzo Cutrona <ilorenzo@cbr11s4.42barcelona.com>
2025-07-25 12:12:43 +0200

    topic/r

--- srcs/topic.cpp
+++ srcs/topic.cpp
@@ -11,27 +11,27 @@ void Server::topic(int fd, std::istringstream &msg){
 
 	if (!isValidChannelName(channelName))
 	{
-		client.clientLog("Bad syntax: Channel names start with '&', '#', '+' or '!' (max length 50)\r\n", RED);
+		client.clientLog("Bad syntax: Channel names start with '&', '#', '+' or '!' (max length 50)", RED);
 		return ;
 	}
 	if (channels.find(channelName) == channels.end()){
-		client.clientLog("Channel [" + channelName + "] doesn't exist. To create one use JOIN <channel>\r\n", RED);
+		client.clientLog("Channel [" + channelName + "] doesn't exist. To create one use JOIN <channel>", RED);
 		return;
 	}
 	std::map<std::string, Channel>::iterator it = channels.find(channelName);
 	if (!it->second.isInChannel(&clients[fd])){
-		client.clientLog("You don't belong to this channel, you can join using JOIN <channel> or getting invited\r\n", RED);
+		client.clientLog("You don't belong to this channel, you can join using JOIN <channel> or getting invited", RED);
 		return;
 	}
 	if (newTopic.empty()){
-		client.clientLog("[" + channelName + "]", MAG);
+		client.clientLog("[" + channelName + "]", MAG, true);
 		client.clientLog(" TOPIC: " + it->second.getTopic() + "\r\n");
 		status = SUCCESS;
 	} else {
 		if (!it->second.isOperator(&clients[fd]) && it->second.getTopicRights())
-			client.clientLog("You need to be an operator of [" + channelName + "] to change its topic\r\n", RED);
+			client.clientLog("You need to be an operator of [" + channelName + "] to change its topic", RED);
 		else if (newTopic[0] != ':')
-			client.clientLog("You forgot to put the ':' before the new topic\r\n", RED);
+			client.clientLog("You forgot to put the ':' before the new topic", RED);
 		else{
 			newTopic = newTopic.substr(1);
 			it->second.setTopic(newTopic);
